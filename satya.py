eaf16e92924ff4c857a87123f561b39ad80adb22
Kodamanchilisatya <142867541+Kodamanchilisatya@users.noreply.github.com>
2024-01-12 10:34:32 +0530

    Update satya.py

--- satya.py
+++ satya.py
@@ -1,4 +1,4 @@
 import streamlit as st
 st.header('rose')
 st.write('rose')
-st.image('https://www.bing.com/ck/a?!&&p=909d88fd6a264f4dJmltdHM9MTcwNDkzMTIwMCZpZ3VpZD0zNzQ1M2ZjZC01ZTFjLTYyNzYtMDEyZS0yY2FkNWZjOTYzYjMmaW5zaWQ9NTY3Mw&ptn=3&ver=2&hsh=3&fclid=37453fcd-5e1c-6276-012e-2cad5fc963b3&u=a1L2ltYWdlcy9zZWFyY2g_cT1yb3NlIGltYWdlJkZPUk09SVFGUkJBJmlkPTNDMDQ0MUI0NDZGRkYxQTU2MzJDN0Y4NjAzNzVGOUU2M0EzRUY1MDI&ntb=1',caption='rose')
+st.image('https://th.bing.com/th?id=OIP.TX7VOHY2KLFt3OcfpnV3HgHaH-&w=240&h=259&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2',caption='rose')
